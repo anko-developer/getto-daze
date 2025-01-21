7525667d0040f0485e9d60c4b1dd2a94eb5b2dac
wook0105 <wook0105@gsretail.com>
2025-01-21 17:03:07 +0900

    [feature] IT-1403 [JIRA] IT-1403 [DESC] 메인 레이아웃 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,10 @@
-import SubTitle from '@/components/common/subTitle/SubTitle';
+import Banner from '@/components/main/Banner';
 import RecommendCont from '@/components/main/RecommendCont';
-import { Box } from '@mui/material';
 
 export default function Main() {
   return (
     <>
-      <Box
-        sx={{
-          height: '140px',
-          marginBottom: '14px',
-          backgroundColor: '#888',
-        }}
-      />
-      <SubTitle title="이달의 추천 입양 동물" href="/recommend" />
+      <Banner />
       <RecommendCont />
     </>
   );
